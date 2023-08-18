2e6574e9e130ad2bb08e332b70e42fa8f030f21e
TheBrutalSkull <thebrutalskullofficiel@hotmail.com>
2023-08-18 18:01:35 -0400

    Basic Ping command with command handler for slash commands

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
+const {
+  Client,
+  GatewayIntentBits,
+  EmbedBuilder,
+  Collection,
+} = require("discord.js");
 const allIntents = [
   GatewayIntentBits.Guilds,
   GatewayIntentBits.GuildMembers,
@@ -9,14 +14,53 @@ const allIntents = [
 const client = new Client({ intents: allIntents });
 const config = require("./private/private.json");
 
-client.on("ready", (ready) => {
-  console.clear();
-  console.log(`Logged as ${client.user.tag}`);
+const fs = require("fs");
+const commandFile = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+const commands = [];
+
+client.commands = new Collection();
+console.clear();
+console.log("\nI loaded :");
+for (const file of commandFile) {
+  const command = require(`./commands/${file}`);
+  console.log(`${file}`);
+  commands.push(command.data.toJSON());
+  client.commands.set(command.data.name, command);
+}
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  //Get the full list of members in the guild**
+  await interaction.guild.members.fetch();
+
+  const exceptionThrowed = new EmbedBuilder()
+    .setColor("Random")
+    .setTitle("Sir, we got a problem here")
+    .setDescription(`Please, contact <@${config.ownerID}>.`)
+    .setTimestamp();
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    exceptionThrowed.addField(`Error`, `${error}`);
+    await interaction.reply({
+      embeds: [exceptionThrowed],
+      ephemeral: true,
+    });
+  }
 });
 
-client.on("messageCreate", (message) => {
-  var content = message.content.toLowerCase();
-  if (content === "hi") message.channel.send("Hi =)");
+client.once("ready", (ready) => {
+  console.log(`\nLogged as ${client.user.tag}`);
 });
 
 client.login(config.token);
